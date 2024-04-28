6a1b0512bbeb159198a42b1083a00ffd785a558a
aiXpertLab <aiXpertLab@gmail.com>
2024-04-28 01:47:53 -0400

    base

--- Home.py
+++ Home.py
@@ -1,52 +1,70 @@
 import streamlit as st, pathlib, os
-from utilities import get_products, aichat
+import utilities as ut
+from cryptography.fernet import Fernet
+import base64
+
 folder = pathlib.Path(__file__).parent.resolve()
 
-st.set_page_config(page_title="Chatbots",  page_icon="🚀",)
-st.write("# 👋 Welcome to Receipt Bot!👋")
-st.markdown(
-    """
+st.set_page_config(page_title="easyStorage: Low-cost storage near you!",  page_icon="🚚",)
+st.image('eslogo.png', use_column_width=True)
+st.write("# 🚚 Low-cost storage near you!🚀")
 
-#### 🚀Intelligent Bookkeeping and Document Management Hub🍨
+openai_api_key = ut.get_key()
 
-
-"""
-)
-
-# st.sidebar.title("Store Spark")
-st.sidebar.image(f"{folder}/resources/sslogo.png", use_column_width=True)
-
-with st.sidebar:
-    # store_link = st.text_input("Enter Your Store URL:",   value="http://hypech.com/StoreSpark", disabled=True, key="store_link")
-    # openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    openai_api_key = os.environ.get("OPENAI_API_KEY")
-    # "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo-0125"
+gpt4 = False
+model="gpt-3.5-turbo-0125",
+if gpt4:
+    openai_api_key = os.environ.get("GPT4_API_KEY")
+    model="gpt-4-turbo",
+print(openai_api_key[:10])
+print(model)
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
     st.session_state.messages.append({"role": "system",      'content': f"""
-            You are ShopBot, an AI assistant for  easyStorage service. 
+            As 'Space Guru', you are an expert in calculating the number of easyPods needed for storage, considering pod dimensions and stored items.  
+            Do not provide detailed explanations or show calculations. 
+            
+            As a start, offer up standard sizes for each item and ask if they should be altered. 
+            
+            When a bed is mentioned, you inquire if it includes a mattress and whether it's a single, double, or king. 
+            For tables, you ask about the seating capacity and if chairs are included. 
+            For chests of drawers, you query the number of drawers, and for wardrobes, the number of doors. 
 
-            Your role is to assist customers in offering various charging and space schedules, providing information, and guiding them through the order process. 
+            These details help you offer a precise estimate of space requirements without delving into packing strategies. 
 
-            Be friendly and helpful in your interactions.
+            You communicate in friendly UK English, providing practical, easy-to-understand advice. 
+            
+            "containers" and "pods" are different. 
 
-            We operate over 85% of the UK and we are adding new locations periodically. Here’s a list of some of the larger towns and cities we cover. Or enter your postcode here to receive a detailed storage quote.
-
-            If you receive an alert, it may mean we do not operate in your area just yet but please call our Storage Solutions Specialists for further information even if your collection address isn’t covered, where you want to store might be.
-
-            Feel free to ask customers about their preferences, recommend products, and inform them about any ongoing promotions.
+            Limited time Promotion: There is usually a 50% discount for the first 12 weeks, call to confirm.
+            
+            Insurance: Protection Cover (previously known as Insurance)	£1.35per week for every £1k of cover.
+            
+            Each easyPod measures Depth: 2100mm, Width: 1500mm, Height: 2240mm (35sqft and 250cuft) with a weight limit of 650kg.  Access with two clear working days notice.
+            Each easyContainer measures Depth: In feet: 19' 4” long x 7' 9” wide x 7' 10” high. In meters: 5.898m long x 2.352m wide x 2.393m high. (160sqft) with no weight limit.  24hr self access available.   
+            
+            If they ask for packing materials say that they can buy these at the time of booking either online at easystorage.com or with the Storage Solutions Specialist when booking on the phone.  
+            
+            Alternatively, they can buy from the Team Leader on the day of collection.  
+            
+            All our vans carry stock.  
+            
+            If asked things secure, let them know that each easyPod is sealed with uniquely numbered seals and then take to purpose-built climate controlled storage facilities with CCTV.  
+            
+            If they have a lot of things that I need to access regularly or they are very heavy suggest that they use an easyContainer.  
+            
+            There are various sizes but the most popular is 20 foot long which is 160sqft internally.  24hr access to these.
 
             The Current various charging and space schedules List is limited as below:
 
-            ```{get_products()}```
+            ```{ut.get_item_sizes()}```
+            ```{ut.get_packing_material_charges()}```
+            ```{ut.get_pod_container_prices()}```
 
-            Make the shopping experience enjoyable and encourage customers to reach out if they have any questions or need assistance.
             """})
+    
     st.session_state.messages.append({"role": "assistant",   "content": "How May I Help You Today💬?"})
 
 
@@ -56,9 +74,9 @@ for message in st.session_state.messages[1:]:
         st.markdown(message["content"])
 
 # React to user input
-if prompt := st.chat_input("💬"):
+if prompt := st.chat_input("easyStorage:💬"):
     if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
+        st.info("OpenAI API Key expired.")
     else: 
         try:
             # Add user message to chat history
@@ -69,11 +87,12 @@ if prompt := st.chat_input("💬"):
 
             # Display assistant response in chat message container
             with st.chat_message("assistant"):
-                stream = aichat(messages = [
+                stream = ut.aichat(messages = [
                         {"role": m["role"], "content": m["content"]}
                         for m in st.session_state.messages
                     ],openai_api_key=openai_api_key)
                 response = st.write_stream(stream)
+                print(response)
             # Add assistant response to chat history
             st.session_state.messages.append({"role": "assistant", "content": response})
         except:
--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
-import requests, openai
+import requests, openai, streamlit as st, pymongo, base64
 from tenacity import retry, wait_random_exponential, stop_after_attempt
+from cryptography.fernet import Fernet
 
 @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
 def aichat(messages, openai_api_key):
@@ -8,8 +9,10 @@ def aichat(messages, openai_api_key):
         response = client.chat.completions.create(
             messages=messages,
             model="gpt-3.5-turbo-0125",
+            max_tokens=2024,
             stream=True,
         )
+        print(response)
         return response
     except Exception as e:
         print("Unable to generate ChatCompletion response")
@@ -27,9 +30,75 @@ def get_products():
         print(f"The store is closed：{response.status_code}")    
 
 
+def get_item_sizes():
+            # ```{ut.get_item_sizes()}```
+            # ```{ut.get_packing_material_charges()}```
+            # ```{ut.get_pod_container_prices()}```
+    url = "https://hypech.com/StoreSpark/easystorage/item_sizes.json"
+    response = requests.get(url)     
+    if response.status_code == 200:  
+        data = response.text                  
+        return data
+    else:
+        print(f"The store is closed：{response.status_code}")    
+
+def get_packing_material_charges():
+    url = "https://hypech.com/StoreSpark/easystorage/packing_material_charges.json"
+    response = requests.get(url)     
+    if response.status_code == 200:  
+        data = response.text                  
+        return data
+    else:
+        print(f"The store is closed：{response.status_code}")    
+
+def get_pod_container_prices():
+    url = "https://hypech.com/StoreSpark/easystorage/pod_container_prices.json"
+    response = requests.get(url)     
+    if response.status_code == 200:  
+        data = response.text                  
+        return data
+    else:
+        print(f"The store is closed：{response.status_code}")    
+
 def get_embedding(text, model="text-embedding-3-small"):
    text = text.replace("\n", " ")
 #    return client.embeddings.create(input = [text], model=model).data[0].embedding
 
 # text = "test embedding"
 # embeddings = get_embedding(text)
+
+def get_key():
+    uri = st.secrets["MONGO_URI"]
+    
+    @st.cache_resource
+    def init_connection():
+        return pymongo.MongoClient(uri)
+
+    client = init_connection()
+
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        mydatabase = client.easystorage
+        mycollection = mydatabase.openai
+        query = {"0.name": "key"}
+        result = mycollection.find_one(query)
+
+        # Check if the document was found
+        openai_api_key = None
+        if result:
+            # Extract the value
+            encrypted_key = result['0']['value']
+            print("Retrieved value:", encrypted_key[:20])
+            st_key = st.secrets["FERNET"]
+            key_bytes = base64.urlsafe_b64decode(st_key)
+            cipher_suite = Fernet(key_bytes)
+            openai_api_key = cipher_suite.decrypt(encrypted_key).decode()
+
+        else:
+            print("Document not found.")
+            
+        return openai_api_key
+    
+    except Exception as e:
+        print(e)
